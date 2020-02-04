828ada88bc3c2a8f95e037df40868b21d3df7a2a
Raja <draja@informatica.com>
2020-02-04 13:27:36 +0530

    Updates

---
+++ precache-manifest.01fd5ca585b072f1d84f50dc04d1f3f4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd314e46524ba15b8c428ee44b24f023",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "13415f87ecabacf954d1",
+    "url": "/ToDo/static/css/2.71d4905c.chunk.css"
+  },
+  {
+    "revision": "fb4335711ebe74779c66",
+    "url": "/ToDo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "13415f87ecabacf954d1",
+    "url": "/ToDo/static/js/2.5a6f83c0.chunk.js"
+  },
+  {
+    "revision": "3159dcd1f0681bdd4921a3bb6680c3d5",
+    "url": "/ToDo/static/js/2.5a6f83c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fb4335711ebe74779c66",
+    "url": "/ToDo/static/js/main.58b254b3.chunk.js"
+  },
+  {
+    "revision": "274c389e001cc617060d",
+    "url": "/ToDo/static/js/runtime-main.bdf4b3bd.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ToDo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ToDo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ToDo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ToDo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ToDo/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
