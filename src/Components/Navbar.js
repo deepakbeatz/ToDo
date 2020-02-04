55e896e326889633ad4d323ce0cba7bd2d2a2cd0
Raja <draja@informatica.com>
2020-02-04 15:05:43 +0530

    initial commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import {Navbar,Nav,NavDropdown,Form,FormControl,Button} from 'react-bootstrap';
+import {Navbar,Nav} from 'react-bootstrap';
 import './navbar.css';
 
 class navbar extends Component{
@@ -8,13 +8,15 @@ class navbar extends Component{
         return(
             <div>
                 <Navbar className="navColor" expand="lg">
-                    <Navbar.Brand href="#home"><i className="fa fa-hand-o-right" aria-hidden="true"></i>ToDo</Navbar.Brand>
+                    <Navbar.Brand href="#"><i className="fa fa-hand-o-right" aria-hidden="true"></i>ToDo</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#">My Tasks</Nav.Link>
-                        <Nav.Link href="#">Logout</Nav.Link>
-                    </Nav>
+                        <Nav className="mr-auto">
+                            <Nav.Link href="#"><i class="fa fa-tasks"></i>MyTasks</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#"><i class="fas fa-sign-out-alt"></i>Logout</Nav.Link>
+                        </Nav>
                     </Navbar.Collapse>
                 </Navbar>
             </div>
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+import * as serviceWorker from './serviceWorker'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
