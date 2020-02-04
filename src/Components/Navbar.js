5ca1f3f926ad2cdcf30b8355c4519db64732ef26
Raja <draja@informatica.com>
2020-02-04 14:40:29 +0530

    initial commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -12,8 +12,8 @@ class navbar extends Component{
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
+                        <Nav.Link href="#">My Tasks</Nav.Link>
+                        <Nav.Link href="#">Logout</Nav.Link>
                     </Nav>
                     </Navbar.Collapse>
                 </Navbar>
